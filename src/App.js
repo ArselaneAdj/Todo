6421edf050d76a954ab4a73588431efa261a6f6e
Your Name <you@example.com>
2023-08-13 21:23:47 +0100

    disappear on delete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
 
-  const[newitem, setNewitem]= useState("")
+  const[newitem, setNewitem]= new Set(useState(""))
 
   const[todo, setTodo]= useState(()=>{
     const localValue = localStorage.getItem("ITEMS")
@@ -17,28 +17,36 @@ function App() {
 
 
   useEffect(()=>{
-    localStorage.setItem("ITEMS", JSON.stringify(todo))
+      localStorage.setItem("ITEMS", JSON.stringify(todo))
   }, [todo])
+
 
   function handleSubmit(e){
     e.preventDefault()
-
+    
     setTodo(currentTodo =>{
-      return [...currentTodo,{id: crypto.randomUUID(), title: newitem, completed: false},]
+      return [...currentTodo,{id: crypto.randomUUID(),title:newitem, completed: false},]
     })
+    
+    
     setNewitem("")
   }
+
+  
 
 
   function toggleTodo(id, completed){
     setTodo(currentTodo =>{
       return currentTodo.map( todo =>{
         if( todo.id === id) {
-          return {...todo, completed}
+          return  {...todo, completed} 
           
         }
         
-        return todo
+        
+          
+        
+        return todo 
       })
     })
   }
@@ -49,7 +57,19 @@ function App() {
     })
   }
 
-  
+  function ra(){
+      var ul = document.getElementById("foo");
+      var items = ul.getElementsByTagName("li");
+        for (var i = 0; i < items.length; ++i) {
+          console.log(items[i].innerHTML.split(""))
+          if(newitem == items[i].innerHTML.split("")[148] ){  
+            return items[i].remove()   
+        }
+        
+      }
+    }
+
+
   
 
   return (
@@ -57,19 +77,19 @@ function App() {
       <form onSubmit={handleSubmit}>
         <div>
           <div class="input-group input-group-lg">
-            <input required placeholder='Add Item' class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" value={newitem} onChange={e => setNewitem(e.target.value)} type='text' id='item'></input><n/>
-            <button class="btn btn-light">add</button>
+            <input required placeholder='Add Item' className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" value={newitem} onChange={e => setNewitem(e.target.value)} type='text' id='item'></input><n/>
+            <button onClick={ra} class="btn btn-light">add</button>
           </div>
         </div>
       </form>
       <h1 class="display-1">todo list</h1>
-      <ul class="list-group list-group-flush">
+      <ul id='foo' class="list-group list-group-flush">
         {todo.length == 0 && "No Todos"}
         {todo.map(todo =>{
-          return(
+          return (
             <li class="list-group-item" key={todo.title}>
-              <label class="display-3">
-                <input class="form-check-input float-left position-absolute top-0 start-0"  type='checkbox' checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}></input>{todo.title}
+              <label  id='cv' class="display-3">
+                <input name='checkbox'  id='cb' className="form-check-input float-left position-absolute top-0 start-0"  type='checkbox' checked={todo.completed} onChange={e =>toggleTodo(todo.id, e.target.checked) }></input>{todo.title}
               </label>
               <button class="btn btn-danger btn-lg  text-center float-left position-absolute top-0 end-0" onClick={() =>del(todo.id)}>delete</button>
             </li>
